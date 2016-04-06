55185a373e8abf7a921d564691223369eb6ffb03
tickbh <tickdream125@hotmail.com>
2016-04-06 16:10:58 +0800

    change the folder

--- src/lib.rs
+++ src/lib.rs
@@ -14,32 +14,26 @@ extern crate td_revent;
 extern crate td_clua_ext;
 
 mod macros;
-mod net_msg;
 mod values;
 mod db;
 mod utils;
-mod event_mgr;
-mod socket_event;
 mod lua_engine;
 mod rp_wrapper;
-mod net_config;
 mod global_config;
 mod redis_wrapper;
 mod lua_custom;
+mod net;
 mod mgr;
 
-pub use net_config::NetConfig;
 pub use global_config::GlobalConfig;
 pub use db::{DbTrait, DbMysql, DbPool, PoolTrait, RedisPool};
-pub use net_msg::NetMsg;
 pub use values::{ErrorKind, NetResult, make_extension_error};
 pub use utils::{FileUtils, ThreadUtils, NetUtils, TelnetUtils, LogUtils};
-pub use socket_event::SocketEvent;
-pub use event_mgr::EventMgr;
 pub use rp_wrapper::{LuaWrapperValue, LuaWrapperVecValue, LuaWrapperTableValue};
 pub use redis_wrapper::{RedisWrapperResult, RedisWrapperCmd, RedisWrapperMsg, RedisWrapperStringVec};
 pub use lua_engine::LuaEngine;
-pub use mgr::{ServiceMgr, HttpMgr, CommandMgr};
+pub use mgr::{ServiceMgr, HttpMgr, CommandMgr, EventMgr};
 pub use lua_custom::register_custom_func;
+pub use net::{NetMsg, NetConfig, SocketEvent};
 
 pub use td_rthreadpool::ThreadPool;
--- src/mgr/mod.rs
+++ src/mgr/mod.rs
@@ -2,7 +2,9 @@
 mod service_mgr;
 mod http_mgr;
 mod command_mgr;
+mod event_mgr;
 
 pub use self::service_mgr::ServiceMgr;
 pub use self::http_mgr::HttpMgr;
 pub use self::command_mgr::CommandMgr;
+pub use self::event_mgr::EventMgr;
