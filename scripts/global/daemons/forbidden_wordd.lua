b4d5c4a641bda3e878a7fd91077c5876dbd0ead2
tickbh <tickdream125@hotmail.com>
2016-04-07 16:51:40 +0800

    添加屏蔽字库

---
+++ scripts/global/daemons/forbidden_wordd.lua
@@ -0,0 +1,46 @@
+--forbidden_wordd.lua
+--屏蔽字库
+--create by wugd
+
+module("FORBIDDEN_WORDD", package.seeall )
+
+local forbidden_table = {}
+local max_length = 1
+
+function get_forbidden_list()
+    return forbidden_table
+end
+
+function load_forbidden_word(file)
+    local fp = io.open(get_full_path(file))
+    forbidden_table = {}
+    if fp then
+        for line in fp:lines() do
+            forbidden_table[line] = true
+            max_length = math.max(max_length, string.len(line))
+        end
+        io.close(fp)
+    end
+end
+
+function has_forbidden_word(content)
+    local wordLen = string.len(content)
+    for len=0, max_length do
+        for i= 1, wordLen - len, 1 do
+            local word = string.sub(content, i, i + len)
+            if forbidden_table[word] then
+                return word
+            end
+        end
+    end
+    return nil
+end
+
+
+function create()         
+    load_forbidden_word("data/txt/banned_word.txt")
+end
+
+create()
+
+
