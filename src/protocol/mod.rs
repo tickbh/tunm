86a1000110ac8853e9f60555a5994675d5200e6e
tickbh <tickdream125@hotmail.com>
2016-10-23 23:23:25 +0800

    add protocol

---
+++ src/protocol/mod.rs
@@ -0,0 +1,18 @@
+
+use td_rlua::{self, Lua, LuaPush};
+
+
+pub trait EngineProtocol: Sized {
+    fn pack_protocol(lua: *mut td_rlua::lua_State, index: i32) -> Option<NetMsg>;
+    fn unpack_message(lua: *mut td_rlua::lua_State, index: i32) -> i32;
+}
+
+mod proto_td;
+mod proto_json;
+mod proto_bin;
+mod proto_text;
+
+pub use self::proto_td::ProtoTd;
+pub use self::proto_json::ProtoJson;
+pub use self::proto_bin::ProtoBin;
+pub use self::proto_text::ProtoText;
