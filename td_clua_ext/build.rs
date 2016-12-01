18cb7c2586a813d89aff46d1c584b21a411a7da1
tickbh <tickdream125@hotmail.com>
2016-12-01 16:12:52 +0800

    fix depend

--- td_clua_ext/build.rs
+++ td_clua_ext/build.rs
@@ -2,11 +2,6 @@ extern crate pkg_config;
 extern crate gcc;
 
 fn main() {
-    match pkg_config::find_library("lua5.2") {
-        Ok(_) => return,
-        Err(..) => {}
-    };
-
     gcc::Config::new()
         .file("cjson/fpconv.c")
         .file("cjson/lua_cjson.c")
